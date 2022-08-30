a63b5b79dfa293b83287231d7baa6839421bd094
Jonathan Finger <jaefinger@gmail.com>
2022-08-29 19:11:29 -0500

    basic bot working!

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import discord
 import os
 from dotenv import load_dotenv
+import random
 
 load_dotenv() 
 
-client = discord.Client(intents=discord.Intents.default())
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot_testing_channel_id = 1013948378251542568
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
 
 client.run(os.getenv('DISCORD_TOKEN'))
-
-print(os.getenv('DISCORD_TOKEN'))
