1d8a7838cb5b1d9e8317df11981d3a7d9548d33c
Jonathan Finger <jaefinger@gmail.com>
2022-09-07 19:25:20 -0500

    adds eta5 test task

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import discord
 import os
 from dotenv import load_dotenv
-from discord.ext import commands
+from discord.ext import commands, tasks
 import random
 import spacy
 from spacy.matcher import Matcher
 import re
+import datetime
 
 # Load env variables
 load_dotenv() 
@@ -161,6 +162,24 @@ async def green_square_bot(message):
             await message.reply(random.choice(exclamations) + f" Thanks for sharing this green square opportunity, {message.author.name}!")
     else:
       ...
+
+# Create tuesday eta5 meeting time and actual meeting time (5pm central)
+tuesday_meeting_time_eta5 = datetime.time(hour=17, minute=55, second=0)
+tuesday_meeting_time = datetime.time(hour=18, minute=0, second=0)
+
+# Create tuesday eta 5 meeting task
+@tasks.loop(time=tuesday_meeting_time_eta5)
+async def eta5_minutes():
+    channel = bot.get_channel(bot_testing_channel_id)
+    await channel.send("ETA 5 minutes until a sniffr meeting, yo!")
+    print("Reminding people that there is a meeting soon")
+
+
+@bot.event
+async def on_ready():
+    if not eta5_minutes.is_running():
+        eta5_minutes.start() #If the task is not already running, start it.
+        print("ETA 5 mins till meeting task started")
       
 # Runs app using Discord token
 bot.run(os.environ['DISCORD_TOKEN'])
